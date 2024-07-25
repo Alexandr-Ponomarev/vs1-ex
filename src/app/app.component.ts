fde8802a932efa2e873cb396a8df03dbba4af56a
Alexander <ponomarevs794@gmail.com>
2024-07-25 17:36:40 +0300

    added console.log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,3 +11,23 @@ import { RouterOutlet } from '@angular/router';
 export class AppComponent {
   title = 'vs1-ex';
 }
+export class ContactFormComponent {
+  name: string = '';
+  email: string = '';
+  telephone: string = '';
+  subject: string = '';
+  message: string = '';
+  numbers: string = '';
+
+  // Флаг для отключения кнопки
+  submitButtonDisabled = true;
+
+  onSubmit() {
+    // Логика отправки формы
+  }
+
+  // Функция, которая проверяет, заполнены ли поля
+  checkFormFields() {
+    this.submitButtonDisabled = !(this.name && this.email && this.telephone && this.subject && this.message && this.numbers);
+  }
+}
